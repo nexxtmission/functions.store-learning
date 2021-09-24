fc2849fc371e4a108527304d26149820094d27e0
LeandroTorresSicilia <jtorressicilia@gmail.com>
2021-09-24 17:51:41 -0400

    fix: return value for addNewUserToFirestore

--- functions/addNewUserToFirestore/services/addUserRecordToFirestore.js
+++ functions/addNewUserToFirestore/services/addUserRecordToFirestore.js
@@ -2,9 +2,11 @@ const firestore = require('./firestore');
 
 const usersCollectionPath = process.env.FIRESTORE_COLLECTION_PATH;
 
-const addUserRecordToFirestore = (userRecord) => {
+const addUserRecordToFirestore = async (userRecord) => {
+    const userData = JSON.parse(JSON.stringify(userRecord));
     const usersCollectionRef = firestore.collection(usersCollectionPath);
-    return usersCollectionRef.add(JSON.parse(JSON.stringify(userRecord)));
+    await usersCollectionRef.add(userData);
+    return userData;
 };
 
 module.exports = addUserRecordToFirestore;
