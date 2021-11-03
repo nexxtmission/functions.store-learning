cf8bad2c7f2ce8d123d26e7b94a4a62a38c18f53
Wilder Gonzalez Diaz <wildergd@gmail.com>
2021-11-03 11:17:33 -0400

    test: add function test

---
+++ functions/deleteUserData/__test__/main.spec.js
@@ -0,0 +1,97 @@
+const deleteUserData = require('../main');
+const getConfig = require('../services/getConfig');
+const deleteDataFromFirestore = require('../services/deleteDataFromFirestore');
+const deleteDataFromRealtimeDatabase = require('../services/deleteDataFromRealtimeDatabase');
+const deleteDataFromStorage = require('../services/deleteDataFromStorage');
+
+const user = { uid: '123' };
+const emptyConfig = {
+    firestorePaths: '',
+    firestoreDeleteRecursive: false,
+    realtimeDatabaseInstance: '',
+    realtimeDatabasePaths: '',
+    storageDefaultBucket: '',
+    storagePaths: '',
+};
+
+jest.mock('../services/deleteDataFromFirestore', () => jest.fn());
+jest.mock('../services/deleteDataFromRealtimeDatabase', () => jest.fn());
+jest.mock('../services/deleteDataFromStorage', () => jest.fn());
+jest.mock('../services/getConfig', () => jest.fn(() => ({ ...emptyConfig })));
+
+describe('deleteUserData()', () => {
+    beforeEach(() => jest.clearAllMocks());
+
+    it('should not delete data', async () => {
+        expect.assertions(3);
+        await deleteUserData(user);
+        expect(deleteDataFromFirestore).not.toHaveBeenCalled();
+        expect(deleteDataFromRealtimeDatabase).not.toHaveBeenCalled();
+        expect(deleteDataFromStorage).not.toHaveBeenCalled();
+    });
+
+    it('should delete data from firestore', async () => {
+        expect.assertions(3);
+        getConfig.mockReturnValue({
+            ...emptyConfig,
+            firestorePaths: 'users/{uid},admin/{uid}',
+        });
+        await deleteUserData(user);
+        expect(deleteDataFromFirestore).toHaveBeenCalledWith({
+            paths: 'users/{uid},admin/{uid}',
+            recursive: false,
+            uid: '123',
+        });
+        expect(deleteDataFromRealtimeDatabase).not.toHaveBeenCalled();
+        expect(deleteDataFromStorage).not.toHaveBeenCalled();
+    });
+
+    it('should delete data from firestore recursivelly', async () => {
+        expect.assertions(3);
+        getConfig.mockReturnValue({
+            ...emptyConfig,
+            firestorePaths: 'users/{uid},admin/{uid}',
+            firestoreDeleteRecursive: true,
+        });
+        await deleteUserData(user);
+        expect(deleteDataFromFirestore).toHaveBeenCalledWith({
+            paths: 'users/{uid},admin/{uid}',
+            recursive: true,
+            uid: '123',
+        });
+        expect(deleteDataFromRealtimeDatabase).not.toHaveBeenCalled();
+        expect(deleteDataFromStorage).not.toHaveBeenCalled();
+    });
+
+    it('should delete data from realtime database', async () => {
+        expect.assertions(3);
+        getConfig.mockReturnValue({
+            ...emptyConfig,
+            realtimeDatabasePaths: 'users/{uid},admin/{uid}',
+        });
+        await deleteUserData(user);
+        expect(deleteDataFromFirestore).not.toHaveBeenCalled();
+        expect(deleteDataFromRealtimeDatabase).toHaveBeenCalledWith({
+            paths: 'users/{uid},admin/{uid}',
+            uid: '123',
+        });
+        expect(deleteDataFromStorage).not.toHaveBeenCalled();
+    });
+
+    it('should delete data from storage', async () => {
+        expect.assertions(3);
+        getConfig.mockReturnValue({
+            ...emptyConfig,
+            storageDefaultBucket: 'bucket_name',
+            storagePaths: '{DEFAULT}/users/profile_{uid}.png,user_files/{uid}',
+        });
+        await deleteUserData(user);
+        expect(deleteDataFromFirestore).not.toHaveBeenCalled();
+        expect(deleteDataFromRealtimeDatabase).not.toHaveBeenCalled();
+        expect(deleteDataFromStorage).toHaveBeenCalledWith({
+            paths: '{DEFAULT}/users/profile_{uid}.png,user_files/{uid}',
+            uid: '123',
+            defaultBucketName: 'bucket_name',
+        });
+    });
+});
