834c9a255bd1355cdad19b670d9ce48fd24b35cb
Wilder Gonzalez Diaz <wildergd@gmail.com>
2021-12-04 01:21:10 -0500

    fix: minor issues

--- functions/validateAddressWithShipEngine/main.js
+++ functions/validateAddressWithShipEngine/main.js
@@ -2,7 +2,7 @@ const mapDataAccordingToSchema = require('./helpers/mapDataAccordingToSchema');
 const validateAddress = require('./services/validateAddress');
 const getConfig = require('./services/getConfig');
 
-const validateAddressWithShipEngine = async (data, context) => {
+const validateAddressWithShipEngine = async (data) => {
     const { fieldsMapping } = getConfig();
     const params = mapDataAccordingToSchema(data, fieldsMapping);
     const result = await validateAddress(params);
